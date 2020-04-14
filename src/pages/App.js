c4f27a5237a09c9e95a65c30e8102d950088a5be
Derly <derlyvivianamurcia@gmail.com>
2020-04-14 00:19:15 -0500

    random quote

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Qoutes from "../components/quote";
+
+function App() {
+  return (
+    <div className="App">
+      <Qoutes />
+    </div>
+  );
+}
+
+export default App;
